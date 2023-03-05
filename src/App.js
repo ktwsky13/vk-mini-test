375edf902dfc91432fee550060433318060d1685
ktwsky13 <42086717+ktwsky13@users.noreply.github.com>
2023-03-05 15:04:26 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import bridge from '@vkontakte/vk-bridge';
-import View from '@vkontakte/vkui/dist/components/View/View';
-import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
@@ -13,13 +12,6 @@ const App = () => {
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
 
 	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data }}) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
 		async function fetchData() {
 			const user = await bridge.send('VKWebAppGetUserInfo');
 			setUser(user);
@@ -33,12 +25,21 @@ const App = () => {
 	};
 
 	return (
-		<View activePanel={activePanel} popout={popout}>
-			<Home id='home' fetchedUser={fetchedUser} go={go} />
-			<Persik id='persik' go={go} />
-		</View>
+		<ConfigProvider>
+			<AdaptivityProvider>
+				<AppRoot>
+					<SplitLayout popout={popout}>
+						<SplitCol>
+							<View activePanel={activePanel}>
+								<Home id='home' fetchedUser={fetchedUser} go={go} />
+								<Persik id='persik' go={go} />
+							</View>
+						</SplitCol>
+					</SplitLayout>
+				</AppRoot>
+			</AdaptivityProvider>
+		</ConfigProvider>
 	);
 }
 
 export default App;
-
